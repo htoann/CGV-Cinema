ca82ca36e3224f40e4f1bc4d82c865c229e10364
HToan <huutrantoan@gmail.com>
2021-08-16 11:08:01 +0700

    DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+namespace Cinema.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
